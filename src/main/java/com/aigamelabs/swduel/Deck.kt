f4168bb480ac910d76b7c0bc8a4d67a5e7b39e0a
Marco Tamassia <nazgul17@gmail.com>
2017-10-14 13:08:18 +1100

    Restructure deck classes

--- src/main/java/com/aigamelabs/swduel/Deck.kt
+++ src/main/java/com/aigamelabs/swduel/Deck.kt
@@ -1,9 +1,24 @@
 package com.aigamelabs.swduel
 
-import com.aigamelabs.swduel.enums.DeckName
-import com.aigamelabs.swduel.Card
-import io.vavr.collection.List
+import java.util.concurrent.ThreadLocalRandom
 
-open class Deck(val deckName: DeckName) {
-    var cards: List<Card> = List.empty()
+import com.aigamelabs.swduel.enums.DeckName
+import io.vavr.collection.Vector
+import io.vavr.control.Try
+
+open class Deck(private val deckName: DeckName, private val cards: Vector<Card>) {
+
+    constructor(deckName: DeckName) : this(deckName, Vector.empty())
+
+    fun drawCard() : Try<Pair<Card, Deck>> {
+        return if (cards.size() > 0) {
+            val card_index = ThreadLocalRandom.current().nextInt(0, cards.size() + 1)
+            val drawnCard = cards[card_index]
+            val newDeck = Deck(deckName, cards.removeAt(card_index))
+            Try.success(Pair(drawnCard, newDeck))
+        }
+        else {
+            Try.failure(Exception("The deck is empty."))
+        }
+    }
 }
