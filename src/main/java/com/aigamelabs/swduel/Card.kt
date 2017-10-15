62d70387f71e348df80f76a7ff7b5602bb2bf7a2
Marco Tamassia <nazgul17@gmail.com>
2017-10-15 13:48:39 +1100

    Simplify class

--- src/main/java/com/aigamelabs/swduel/Card.kt
+++ src/main/java/com/aigamelabs/swduel/Card.kt
@@ -3,169 +3,123 @@ package com.aigamelabs.swduel
 import com.aigamelabs.swduel.enums.Formula
 import com.aigamelabs.swduel.enums.*
 import io.vavr.collection.HashMap
+import io.vavr.collection.HashSet
 
-data class Card(val cardGroup: CardGroup, val name: String, val color: CardColor,
-           val resourceCost: HashMap<Resource, Int>, val coinCost: Int,
-           val linkingSymbol: LinkingSymbol, val linksTo: LinkingSymbol,
-           val tradingBonuses: Set<Resource>, val resourceProduction: HashMap<Resource, Int>,
-           val resourceAlternativeProduction: ResourcesAlternative,
-           val victoryPoints: Int, val victoryPointsFormula: Formula,
-           val coinsProduced: Int, val coinsProducedFormula: Formula,
-           val referenceCity: CityForFormula,
-           val scienceSymbol: ScienceSymbol, val militaryPoints: Int,
-           val bonuses: Set<Bonus>
-           ) {
+data class Card(
+        val cardGroup: CardGroup,
+        val color: CardColor,
+        val name: String,
+        val resourceCost: HashMap<Resource, Int> = HashMap.empty(),
+        val coinCost: Int = 0,
+        val linkingSymbol: LinkingSymbol = LinkingSymbol.NONE,
+        val linksTo: LinkingSymbol = LinkingSymbol.NONE,
+        val tradingBonuses: HashSet<Resource> = HashSet.empty(),
+        val resourceProduction: HashMap<Resource, Int> = HashMap.empty(),
+        val resourceAlternativeProduction: ResourcesAlternative = ResourcesAlternative.NONE,
+        val victoryPoints: Int = 0,
+        val victoryPointsFormula: Formula = Formula.ABSOLUTE,
+        val coinsProduced: Int = 0,
+        val coinsProducedFormula: Formula = Formula.ABSOLUTE,
+        val referenceCity: CityForFormula = CityForFormula.NOT_APPLICABLE,
+        val scienceSymbol: ScienceSymbol = ScienceSymbol.NONE,
+        val militaryPoints: Int = 0,
+        val bonuses: Set<Bonus> = emptySet()
+) {
     
     // Constructor for (red) military cards
-    constructor(cardGroup: CardGroup, name: String, resourceCost: HashMap<Resource, Int>, coinCost: Int,
+    constructor(cardGroup: CardGroup, name: String, resourceCost: Map<Resource, Int>, coinCost: Int,
                 linkingSymbol: LinkingSymbol, linksTo: LinkingSymbol, militaryPoints: Int) :
             this(cardGroup = cardGroup, name = name, color = CardColor.RED,
-                    resourceCost = resourceCost, coinCost = coinCost,
+                    resourceCost = HashMap.ofAll(resourceCost), coinCost = coinCost,
                     linkingSymbol = linkingSymbol, linksTo = linksTo,
-                    tradingBonuses = emptySet<Resource>(), resourceProduction = HashMap.empty(),
-                    resourceAlternativeProduction = ResourcesAlternative.NONE,
-                    victoryPoints = 0, victoryPointsFormula = Formula.ABSOLUTE,
-                    coinsProduced = 0, coinsProducedFormula = Formula.ABSOLUTE,
-                    referenceCity = CityForFormula.NOT_APPLICABLE,
-                    scienceSymbol = ScienceSymbol.NONE, militaryPoints = militaryPoints,
-                    bonuses = emptySet<Bonus>()
+                    militaryPoints = militaryPoints
             )
 
     // Constructor for (green) science cards
-    constructor(cardGroup: CardGroup, name: String, resourceCost: HashMap<Resource, Int>, coinCost: Int,
+    constructor(cardGroup: CardGroup, name: String, resourceCost: Map<Resource, Int>, coinCost: Int,
                 linkingSymbol: LinkingSymbol, linksTo: LinkingSymbol,
                 victoryPoints: Int, scienceSymbol: ScienceSymbol) :
             this(cardGroup = cardGroup, name = name, color = CardColor.GREEN,
-                    resourceCost = resourceCost, coinCost = coinCost,
+                    resourceCost = HashMap.ofAll(resourceCost), coinCost = coinCost,
                     linkingSymbol = linkingSymbol, linksTo = linksTo,
-                    tradingBonuses = emptySet<Resource>(), resourceProduction = HashMap.empty(),
-                    resourceAlternativeProduction = ResourcesAlternative.NONE,
-                    victoryPoints = victoryPoints, victoryPointsFormula = Formula.ABSOLUTE,
-                    coinsProduced = 0, coinsProducedFormula = Formula.ABSOLUTE,
-                    referenceCity = CityForFormula.NOT_APPLICABLE,
-                    scienceSymbol = scienceSymbol, militaryPoints = 0,
-                    bonuses = emptySet<Bonus>()
+                    victoryPoints = victoryPoints,
+                    scienceSymbol = scienceSymbol
             )
 
     // Constructor for (brown/gray) resources cards
-    constructor(cardGroup: CardGroup, name: String, color: CardColor, coinCost: Int, resourcesProduced: HashMap<Resource, Int>) :
-            this(cardGroup = cardGroup, name = name, color = color,
-                    resourceCost = HashMap.empty(), coinCost = coinCost,
-                    linkingSymbol = LinkingSymbol.NONE, linksTo = LinkingSymbol.NONE,
-                    tradingBonuses = emptySet<Resource>(), resourceProduction = resourcesProduced,
-                    resourceAlternativeProduction = ResourcesAlternative.NONE,
-                    victoryPoints = 0, victoryPointsFormula = Formula.ABSOLUTE,
-                    coinsProduced = 0, coinsProducedFormula = Formula.ABSOLUTE,
-                    referenceCity = CityForFormula.NOT_APPLICABLE,
-                    scienceSymbol = ScienceSymbol.NONE, militaryPoints = 0,
-                    bonuses = emptySet<Bonus>()
+    constructor(cardGroup: CardGroup, name: String, color: CardColor, coinCost: Int, resourcesProduced: Map<Resource, Int>) :
+            this(cardGroup = cardGroup, name = name, color = color, coinCost = coinCost,
+                    resourceProduction = HashMap.ofAll(resourcesProduced)
             )
 
     // Constructor for (golden) trading cards
     constructor(cardGroup: CardGroup, name: String, coinCost: Int, tradingBonuses: Set<Resource>) :
-            this(cardGroup = cardGroup, name = name, color = CardColor.GOLD,
-                    resourceCost = HashMap.empty(), coinCost = coinCost,
-                    linkingSymbol = LinkingSymbol.NONE, linksTo = LinkingSymbol.NONE,
-                    tradingBonuses = tradingBonuses, resourceProduction = HashMap.empty(),
-                    resourceAlternativeProduction = ResourcesAlternative.NONE,
-                    victoryPoints = 0, victoryPointsFormula = Formula.ABSOLUTE,
-                    coinsProduced = 0, coinsProducedFormula = Formula.ABSOLUTE,
-                    referenceCity = CityForFormula.NOT_APPLICABLE,
-                    scienceSymbol = ScienceSymbol.NONE, militaryPoints = 0,
-                    bonuses = emptySet<Bonus>()
+            this(cardGroup = cardGroup, name = name, color = CardColor.GOLD, coinCost = coinCost,
+                    tradingBonuses = HashSet.ofAll(tradingBonuses)
             )
 
     // Constructor for (golden) coin cards
     constructor(cardGroup: CardGroup, name: String, coinsProduced: Int, linkingSymbol: LinkingSymbol) :
             this(cardGroup = cardGroup, name = name, color = CardColor.GOLD,
-                    resourceCost = HashMap.empty(), coinCost = 0,
-                    linkingSymbol = linkingSymbol, linksTo = LinkingSymbol.NONE,
-                    tradingBonuses = emptySet<Resource>(), resourceProduction = HashMap.empty(),
-                    resourceAlternativeProduction = ResourcesAlternative.NONE,
-                    victoryPoints = 0, victoryPointsFormula = Formula.ABSOLUTE,
-                    coinsProduced = coinsProduced, coinsProducedFormula = Formula.ABSOLUTE,
-                    referenceCity = CityForFormula.NOT_APPLICABLE,
-                    scienceSymbol = ScienceSymbol.NONE, militaryPoints = 0,
-                    bonuses = emptySet<Bonus>()
+                    linkingSymbol = linkingSymbol,
+                    coinsProduced = coinsProduced
             )
 
     // Constructor for (golden) alternative production cards
-    constructor(cardGroup: CardGroup, name: String, resourceCost: HashMap<Resource, Int>, coinCost: Int,
+    constructor(cardGroup: CardGroup, name: String, resourceCost: Map<Resource, Int>, coinCost: Int,
                 resourceAlternativeProduction: ResourcesAlternative) :
             this(cardGroup = cardGroup, name = name, color = CardColor.GOLD,
-                    resourceCost = resourceCost, coinCost = coinCost,
-                    linkingSymbol = LinkingSymbol.NONE, linksTo = LinkingSymbol.NONE,
-                    tradingBonuses = emptySet<Resource>(), resourceProduction = HashMap.empty(),
-                    resourceAlternativeProduction = resourceAlternativeProduction,
-                    victoryPoints = 0, victoryPointsFormula = Formula.ABSOLUTE,
-                    coinsProduced = 0, coinsProducedFormula = Formula.ABSOLUTE,
-                    referenceCity = CityForFormula.NOT_APPLICABLE,
-                    scienceSymbol = ScienceSymbol.NONE, militaryPoints = 0,
-                    bonuses = emptySet<Bonus>()
+                    resourceCost = HashMap.ofAll(resourceCost), coinCost = coinCost,
+                    resourceAlternativeProduction = resourceAlternativeProduction
             )
 
     // Constructor for (golden) bonus cards
-    constructor(cardGroup: CardGroup, name: String, resourceCost: HashMap<Resource, Int>, linksTo: LinkingSymbol,
+    constructor(cardGroup: CardGroup, name: String, resourceCost: Map<Resource, Int>, linksTo: LinkingSymbol,
                 victoryPoints: Int, coinsProduced: Int, coinsProducedFormula: Formula) :
             this(cardGroup = cardGroup, name = name, color = CardColor.GOLD,
-                    resourceCost = resourceCost, coinCost = 0,
-                    linkingSymbol = LinkingSymbol.NONE, linksTo = linksTo,
-                    tradingBonuses = emptySet<Resource>(), resourceProduction = HashMap.empty(),
-                    resourceAlternativeProduction = ResourcesAlternative.NONE,
-                    victoryPoints = victoryPoints, victoryPointsFormula = Formula.ABSOLUTE,
+                    resourceCost = HashMap.ofAll(resourceCost),
+                    linksTo = linksTo,
+                    victoryPoints = victoryPoints,
                     coinsProduced = coinsProduced, coinsProducedFormula = coinsProducedFormula,
-                    referenceCity = CityForFormula.YOUR_CITY,
-                    scienceSymbol = ScienceSymbol.NONE, militaryPoints = 0,
-                    bonuses = emptySet<Bonus>()
+                    referenceCity = CityForFormula.YOUR_CITY
             )
 
     // Constructor for (blue) victory points cards
-    constructor(cardGroup: CardGroup, name: String, resourceCost: HashMap<Resource, Int>,
+    constructor(cardGroup: CardGroup, name: String, resourceCost: Map<Resource, Int>,
                 linkingSymbol: LinkingSymbol, linksTo: LinkingSymbol,
                 victoryPoints: Int) :
             this(cardGroup = cardGroup, name = name, color = CardColor.GOLD,
-                    resourceCost = resourceCost, coinCost = 0,
+                    resourceCost = HashMap.ofAll(resourceCost),
                     linkingSymbol = linkingSymbol, linksTo = linksTo,
-                    tradingBonuses = emptySet<Resource>(), resourceProduction = HashMap.empty(),
-                    resourceAlternativeProduction = ResourcesAlternative.NONE,
-                    victoryPoints = victoryPoints, victoryPointsFormula = Formula.ABSOLUTE,
-                    coinsProduced = 0, coinsProducedFormula = Formula.ABSOLUTE,
-                    referenceCity = CityForFormula.NOT_APPLICABLE,
-                    scienceSymbol = ScienceSymbol.NONE, militaryPoints = 0,
-                    bonuses = emptySet<Bonus>()
+                    victoryPoints = victoryPoints
             )
 
     // Constructor for (purple) bonus cards
-    constructor(cardGroup: CardGroup, name: String, resourceCost: HashMap<Resource, Int>,
+    constructor(cardGroup: CardGroup, name: String, resourceCost: Map<Resource, Int>,
                 victoryPoints: Int, victoryPointsFormula: Formula,
                 coinsProduced: Int, coinsProducedFormula: Formula) :
             this(cardGroup = cardGroup, name = name, color = CardColor.GOLD,
-                    resourceCost = resourceCost, coinCost = 0,
-                    linkingSymbol = LinkingSymbol.NONE, linksTo = LinkingSymbol.NONE,
-                    tradingBonuses = emptySet<Resource>(), resourceProduction = HashMap.empty(),
-                    resourceAlternativeProduction = ResourcesAlternative.NONE,
+                    resourceCost = HashMap.ofAll(resourceCost),
                     victoryPoints = victoryPoints, victoryPointsFormula = victoryPointsFormula,
                     coinsProduced = coinsProduced, coinsProducedFormula = coinsProducedFormula,
                     referenceCity = CityForFormula.CITY_WITH_MOST_UNITS,
-                    scienceSymbol = ScienceSymbol.NONE, militaryPoints = 0,
+                    militaryPoints = 0,
                     bonuses = emptySet<Bonus>()
             )
 
     // Constructor for (wonder) bonus cards
-    constructor(cardGroup: CardGroup, name: String, resourceCost: HashMap<Resource, Int>,
+    constructor(cardGroup: CardGroup, name: String, resourceCost: Map<Resource, Int>,
                 resourceAlternativeProduction: ResourcesAlternative,
                 coinsProduced: Int, victoryPoints: Int,
                 militaryPoints: Int,
                 bonuses: Set<Bonus>) :
             this(cardGroup = cardGroup, name = name, color = CardColor.WONDER,
-                    resourceCost = resourceCost, coinCost = 0,
-                    linkingSymbol = LinkingSymbol.NONE, linksTo = LinkingSymbol.NONE,
-                    tradingBonuses = emptySet<Resource>(), resourceProduction = HashMap.empty(),
+                    resourceCost = HashMap.ofAll(resourceCost),
                     resourceAlternativeProduction = resourceAlternativeProduction,
-                    victoryPoints = victoryPoints, victoryPointsFormula = Formula.ABSOLUTE,
-                    coinsProduced = coinsProduced, coinsProducedFormula = Formula.ABSOLUTE,
+                    victoryPoints = victoryPoints,
+                    coinsProduced = coinsProduced,
                     referenceCity = CityForFormula.CITY_WITH_MOST_UNITS,
-                    scienceSymbol = ScienceSymbol.NONE, militaryPoints = militaryPoints,
+                    militaryPoints = militaryPoints,
                     bonuses = bonuses
             )
 }
