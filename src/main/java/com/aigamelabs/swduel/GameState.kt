0dc0e344d326b564a56f1500f22915b601172556
Marco Tamassia <nazgul17@gmail.com>
2017-10-15 13:11:41 +1100

    Implement GameStateFactory

--- src/main/java/com/aigamelabs/swduel/GameState.kt
+++ src/main/java/com/aigamelabs/swduel/GameState.kt
@@ -2,36 +2,16 @@ package com.aigamelabs.swduel
 
 import com.aigamelabs.swduel.enums.ProgressToken
 import io.vavr.collection.HashSet
-import java.util.*
 
-class GameState (
-        var firstAgeDeck : Deck,
-        var secondAgeDeck : Deck,
-        var thirdAgeDeck : Deck,
-        var wondersDeck : Deck,
-        var burnedCardsDeck : Deck,
-        var currentGraph : Graph<Card>?,
-        var progressTokens : HashSet<ProgressToken>,
-        var militaryBoard: MilitaryBoard
-) {
-
-    constructor() : this(
-            DeckFactory.createFirstAgeDeck(),
-            DeckFactory.createSecondAgeDeck(),
-            DeckFactory.createThirdAgeDeck(),
-            DeckFactory.createWondersDeck(),
-            Deck("Burned"),
-            null,
-            HashSet.empty(),
-            MilitaryBoard()
-    ) {
-        // Setup graph for first age
-        val graphCreationOutcome = GraphFactory.makeFirstAgeGraph(firstAgeDeck)
-        currentGraph = graphCreationOutcome.first
-        firstAgeDeck = graphCreationOutcome.second
-        // Setup progress tokens
-        val allTokens = ProgressToken.values().toMutableList()
-        Collections.shuffle(allTokens)
-        progressTokens.addAll(allTokens.subList(0, 5))
-    }
-}
+data class GameState (
+        val firstAgeDeck : Deck,
+        val secondAgeDeck : Deck,
+        val thirdAgeDeck : Deck,
+        val wondersDeck : Deck,
+        val burnedCardsDeck : Deck,
+        val currentGraph : Graph<Card>?,
+        val progressTokens : HashSet<ProgressToken>,
+        val militaryBoard: MilitaryBoard,
+        val player1City : PlayerCity,
+        val player2City : PlayerCity
+)
---
+++ src/main/java/com/aigamelabs/swduel/GameStateFactory.kt
@@ -0,0 +1,41 @@
+package com.aigamelabs.swduel
+
+import com.aigamelabs.swduel.enums.ProgressToken
+import io.vavr.collection.HashSet
+import java.util.*
+
+object GameStateFactory {
+
+    fun createNewGameState() : GameState {
+        return createNewGameState("P1", "P2")
+    }
+
+    fun createNewGameState(p1Name : String, p2Name : String) : GameState {
+
+        // Initialize decks
+        var firstAgeDeck = DeckFactory.createFirstAgeDeck()
+        val secondAgeDeck = DeckFactory.createSecondAgeDeck()
+        val thirdAgeDeck = DeckFactory.createThirdAgeDeck()
+        val wondersDeck = DeckFactory.createWondersDeck()
+        val burnedCardsDeck = Deck("Burned")
+
+        // Setup graph for first age
+        val graphCreationOutcome = GraphFactory.makeFirstAgeGraph(firstAgeDeck)
+        val currentGraph = graphCreationOutcome.first
+        firstAgeDeck = graphCreationOutcome.second
+
+        // Setup progress tokens
+        val allTokens = ProgressToken.values().toMutableList()
+        Collections.shuffle(allTokens)
+        val progressTokens = HashSet.ofAll(allTokens.subList(0, 5))
+
+        // Set cities
+        var player1City = PlayerCity(p1Name)
+        var player2City = PlayerCity(p2Name)
+        player1City = player1City.setOpponentCity(player2City)
+        player2City = player2City.setOpponentCity(player1City)
+
+        return GameState(firstAgeDeck, secondAgeDeck, thirdAgeDeck, wondersDeck, burnedCardsDeck, currentGraph,
+                progressTokens, MilitaryBoard(), player1City, player2City)
+    }
+}
