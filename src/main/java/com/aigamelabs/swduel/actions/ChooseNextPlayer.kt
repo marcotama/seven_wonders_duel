d8eade154886320a4f5f26ce18132fdca7f443e9
Marco Tamassia <nazgul17@gmail.com>
2017-10-21 18:36:49 +1100

    Implement choose next player action

--- src/main/java/com/aigamelabs/swduel/actions/ChooseNextPlayer.kt
+++ src/main/java/com/aigamelabs/swduel/actions/ChooseNextPlayer.kt
@@ -1,11 +1,12 @@
 package com.aigamelabs.swduel.actions
 
+import com.aigamelabs.swduel.DecisionFactory
 import com.aigamelabs.swduel.GameState
-import com.aigamelabs.swduel.GameStateFactory
 import com.aigamelabs.swduel.enums.PlayerTurn
 
-class ChooseNextPlayer(playerTurn: PlayerTurn, val nextTurn : PlayerTurn) : Action(playerTurn) {
+class ChooseNextPlayer(playerTurn: PlayerTurn) : Action(playerTurn) {
     override fun process(gameState: GameState) : GameState {
-        return GameStateFactory.createNewGameState() // TODO
+        val decision = DecisionFactory.makeTurnDecision(playerTurn, gameState, true)
+        return gameState.update(decisionQueue_ = gameState.decisionQueue.enqueue(decision))
     }
 }
