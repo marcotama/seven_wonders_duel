349827c0bfcb864aaf025fdded30e546c35f2cfe
Marco Tamassia <nazgul17@gmail.com>
2017-10-22 19:20:37 +1100

    Add output for game outcome

--- src/main/kotlin/com/aigamelabs/swduel/Game.kt
+++ src/main/kotlin/com/aigamelabs/swduel/Game.kt
@@ -1,6 +1,8 @@
 package com.aigamelabs.swduel
 
+import com.aigamelabs.swduel.enums.GamePhase
 import com.aigamelabs.swduel.enums.PlayerTurn
+import io.vavr.collection.HashSet
 import java.util.*
 
 class Game(private val players : HashMap<PlayerTurn,Player>) {
@@ -30,6 +32,20 @@ class Game(private val players : HashMap<PlayerTurn,Player>) {
             if (gameState.cardStructure!!.isEmpty()) {
                 gameState = gameState.switchToNextAge(generator)
             }
+        }
+
+        val endGamePhases = HashSet.of(GamePhase.MILITARY_SUPREMACY, GamePhase.SCIENCE_SUPREMACY, GamePhase.CIVILIAN_VICTORY)
+        if (endGamePhases.contains(gameState.gamePhase)) {
+            val p1VictoryPoints = gameState.calculateVictoryPoints(PlayerTurn.PLAYER_1)
+            val p2VictoryPoints = gameState.calculateVictoryPoints(PlayerTurn.PLAYER_2)
+            when {
+                p1VictoryPoints > p2VictoryPoints -> System.out.println("Player 1 wins with $p1VictoryPoints versus $p2VictoryPoints")
+                p2VictoryPoints > p1VictoryPoints -> System.out.println("Player 2 wins with $p1VictoryPoints versus $p2VictoryPoints")
+                else -> System.out.println("Players scored the same amount of points: $p1VictoryPoints")
+            }
+        }
+        else {
+            throw Exception("Something went wrong: we left the game loop but the game phase is not final:\n${gameState.gamePhase}")
         }
     }
 }
